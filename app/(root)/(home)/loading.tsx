55dafac8741e1f439b55a712e35518ec2822af0c
Lautaro Epifani <epifanilautaro@gmail.com>
2024-02-21 11:37:15 +0100

    check

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -5,14 +5,14 @@ const loading = () => {
   return (
     <main className='flex-center paddings mx-auto w-full max-w-screen-2xl flex-col'>
       <section className='nav-padding w-full'>
-        <Skeleton className='w-full h-[274px] rounded-lg bg-red-500' />
+        <Skeleton className='w-full h-[274px] rounded-lg bg-black-200/40' />
       </section>
       <section className='nav-padding w-full'>
-        <Skeleton className='w-56 h-10' />
+        <Skeleton className='w-56 h-10 bg-black-200/40' />
         <div className='mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start'>
-        <Skeleton className='w-full sm:w-[356px] h-[440px] rounded-lg' />
-        <Skeleton className='w-full sm:w-[356px] h-[440px] rounded-lg' />
-        <Skeleton className='w-full sm:w-[356px] h-[440px] rounded-lg' />  
+        <Skeleton className='w-full sm:w-[356px] h-[440px] rounded-lg bg-black-200/40' />
+        <Skeleton className='w-full sm:w-[356px] h-[440px] rounded-lg bg-black-200/40' />
+        <Skeleton className='w-full sm:w-[356px] h-[440px] rounded-lg bg-black-200/40' />  
         </div>
       </section>
     </main>
